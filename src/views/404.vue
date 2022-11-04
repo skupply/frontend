9b3a91866e0926167e50ecad1333afa0dad9729a
najirod02 <dorijan.dizepp@gmail.com>
2022-11-04 17:31:02 +0100

    added 404 and 500 pages and images

---
+++ src/views/404.vue
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<template>
+
+<!--inserire navigation bar-->
+
+<n-space vertical size="medium" style="padding: 100px 0px 100px 0px" justify="space-evenly" align="center">
+    <n-layout align="center">
+        <n-layout-content style="padding-bottom:15vh">
+            <img src="../images/404.png" style="max-width:auto; max-height:auto">
+        </n-layout-content>
+        <n-layout-content>
+            <n-space vertical>
+                <span class="ag-t-h1">Uh? Sembra che la pagina non esista</span>
+                <span class="ag-t-large">Il team di Skupply si scusa per il disagio. Visita il negozio per riprendere la tua esperienza!</span>
+            </n-space>
+        </n-layout-content>
+        <n-layout-content style="padding-top:5vh">
+            <n-button round size="large" type="primary" block>Visita il negozio</n-button>
+        </n-layout-content>
+    </n-layout>
+</n-space>
+
+<!--inserire footer-->
+
+</template>
+
+<style scoped>
+</style>
