99576cd6b0f9d80bf1d8ee71e37e1bbd4ae67ffa
najirod02 <dorijan.dizepp@gmail.com>
2022-11-24 22:02:22 +0100

    added authentication for pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '../stores/user'
 
 import Home from '../views/Home.vue'
 import NotFound from '../views/404.vue'
@@ -9,37 +10,60 @@ import Profile from '../views/Profile.vue'
 import Cart from '../views/Cart.vue'
 import Market from '../views/Market.vue'
 
+
+// no authentication required
+const noAuthenticationRequired = (to, from, next) => {
+    next()
+  }
+  
+// make sure user is authenticated
+const requiresAuthentication = (to, from, next) => {
+    const userStore = useUserStore();
+    if (userStore.token != null) {
+        return next()   
+    }
+
+    // not successful
+    next('/login')
+}
+
 const router = createRouter({
     history: createWebHistory(
         import.meta.env.BASE_URL),
     routes: [{
             path: '/',
             name: 'home',
+            beforeEnter: noAuthenticationRequired,
             component: Home
         },
         {
             path: '/login',
             name: 'login',
+            beforeEnter: noAuthenticationRequired,
             component: Login
         },
         {
             path: '/signup',
             name: 'signup',
+            beforeEnter: noAuthenticationRequired,
             component: Signup
         },
         {
             path: '/profile',
             name: 'profile',
+            beforeEnter: requiresAuthentication,
             component: Profile
         },
         {
             path: '/cart',
             name: 'cart',
+            beforeEnter: requiresAuthentication,
             component: Cart
         },
         {
             path: '/market',
             name: 'market',
+            beforeEnter: noAuthenticationRequired,
             component: Market
         },
         {
--- src/stores/user.js
+++ src/stores/user.js
@@ -2,23 +2,36 @@ import { defineStore } from 'pinia';
 import { useStorage } from '@vueuse/core'
 
 //il reset porta allo stato iniziale ovverro valori a null
+//come definito qui sotto in defaultState
 const defaultState = {
   token: null, 
-  email: null
+  email: null,
+  username: null,
 }
 
 export const useUserStore = defineStore('user', {
   state: () => {
     return { 
       token: useStorage('token', null), 
-      email: useStorage('email', null)
+      email: useStorage('email', null),
+      username: useStorage('username', null),
     };
   },
   actions: {
+    //setters
     setToken(token) { this.token = token; },
     setEmail(email) { this.email = email; },
+    setUsername(username) { this.username = username; },
+
+    //getters
+    getToken() {return this.token},
+    getEmail() { return this.email},
+    getUsername() { return this.username},
+
+    //others
     reset() {Object.assign(this, defaultState)},
   },
+  //sessione persistente nelle pagine
   persist: {
     enabled: true
   }
